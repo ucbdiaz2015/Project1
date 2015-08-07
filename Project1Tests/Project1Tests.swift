7423b47927aa2da25fcc8859e1f184a0d8c8111a
Stepan Grigoryan <stepang@gmail.com>
2015-08-07 10:30:06 -0700

    Initial Commit

---
+++ Project1Tests/Project1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Project1Tests.swift
+//  Project1Tests
+//
+//  Created by Stepan Grigoryan on 8/7/15.
+//  Copyright © 2015 Stepan Grigoryan. All rights reserved.
+//
+
+import XCTest
+@testable import Project1
+
+class Project1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
