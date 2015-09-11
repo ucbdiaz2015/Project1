2a21289f6272560c5fa4a55b965691bfbf63554c
Cynthia <ucbdiaz2015@berkeley.edu>
2015-09-11 05:23:35 -0700

    completed

--- Project1/Project1.swift
+++ Project1/Project1.swift
@@ -1,14 +1,77 @@
 func nextCellStates(currentStates: [[Bool]]) -> [[Bool]] {
     // IMPLEMENT ME
     // Take a look at https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life for the rules
-    return currentStates
+    
+    
+    var newStates = Array(count: currentStates.count, repeatedValue: Array(count: currentStates[0].count, repeatedValue: Bool()))
+    //[[Bool]]()
+    
+    var i = 0
+    for cell in currentStates {
+        var j = 0
+        for cell2 in cell {
+            var neighbors = 0
+            //check left, right, up, and down
+            if (j+1<cell.count && currentStates[i][j+1]) {
+                neighbors += 1;
+            }
+            if (j-1>=0 && currentStates[i][j-1]) {
+                neighbors += 1;
+            }
+            if (i+1<currentStates.count && currentStates[i+1][j]) {
+                neighbors += 1;
+            }
+            if (i-1>=0 && currentStates[i-1][j]) {
+                neighbors += 1;
+            }
+            
+            //check diagonal neighbors
+            if (j+1<cell.count && i+1<currentStates.count && currentStates[i+1][j+1]) {
+                neighbors += 1;
+            }
+            if (j-1>=0 && i+1<currentStates.count && currentStates[i+1][j-1]) {
+                neighbors += 1;
+            }
+            if (j+1<cell.count && i-1>=0 && currentStates[i-1][j+1]) {
+                neighbors += 1;
+            }
+            if (j-1>=0 && i-1>=0 && currentStates[i-1][j-1]) {
+                neighbors += 1;
+            }
+            
+            
+            
+            if (cell2) {
+                if (neighbors < 2 || neighbors > 3) {
+                    newStates[i][j] = false
+                } else {
+                    newStates[i][j] = true
+                }
+            } else {
+                if (neighbors == 3) {
+                    newStates[i][j] = true
+                } else {
+                    newStates[i][j] = false
+                }
+            }
+            j += 1
+        }
+        i += 1
+    }
+    
+    return newStates
 }
+
 
 class LRUCache<K:Hashable, V> {
     private var capacity: Int
+    private var pairs: Dictionary<K, (V,Int)>
+    private var time: Int
 
     init(capacity: Int) {
         self.capacity = capacity
+        self.pairs = Dictionary<K,(V,Int)>()
+        self.time = 0
     }
 
     func get(k: K) -> V? {
@@ -17,7 +80,14 @@ class LRUCache<K:Hashable, V> {
         /*
         *  Get the value of the key if the key exists in the cache, otherwise return nil.
         */
-
+        
+        //if key exists, move it to front of cache & return its value, 
+        //else remvoe LRU & insert k,v to front of cache (maybe not?)
+        if let kVal = pairs[k] {
+            pairs[k] = (kVal.0,time)
+            time++
+            return kVal.0
+        }
         return nil
     }
 
@@ -29,5 +99,27 @@ class LRUCache<K:Hashable, V> {
         * When the cache reached its capacity, it should invalidate the 
         * least recently used item before inserting a new item.
         */
+        if get(k) == nil && capacity > 0 { //only if k not present
+            
+        
+            var minTime: Int = time
+            var minKey: K?
+            if pairs.count == capacity {
+                for pair in pairs {
+                    if pair.1.1 < minTime {
+                        minTime = pair.1.1
+                        minKey = pair.0
+                    }
+                }
+                if let min = minKey {
+                    pairs[min] = nil
+                }
+            
+            }
+            pairs[k] = (v, time)
+            time++
+        } else if capacity < 1 {
+            pairs[k] = nil
+        }
     }
 }
--- Project1Tests/Project1Tests.swift
+++ Project1Tests/Project1Tests.swift
@@ -31,6 +31,62 @@ class GOLTests: XCTestCase {
         ]
         XCTAssertEqual(expected, nextStates)
     }
+    
+    func testGol2() {
+        let nextStates: [[Bool]] = nextCellStates([
+            [false, true, false],
+            [false, true, false],
+            [false, true, false]
+        ])
+        let expected: [[Bool]] = [
+            [false, false, false],
+            [true, true, true],
+            [false, false, false]
+        ]
+        XCTAssertEqual(expected, nextStates)
+    }
+    
+    func testGol3() {
+        let nextStates: [[Bool]] = nextCellStates([
+            [false, false, false],
+            [true, true, true],
+            [false, false, false]
+            ])
+        let expected: [[Bool]] = [
+            [false, true, false],
+            [false, true, false],
+            [false, true, false]
+        ]
+        XCTAssertEqual(expected, nextStates)
+    }
+    
+    func testGol4() {
+        let nextStates: [[Bool]] = nextCellStates([
+            [true]
+            ])
+        let expected: [[Bool]] = [
+            [false]
+        ]
+        
+        XCTAssertEqual(expected, nextStates)
+    }
+    
+    func testGol5() {
+        let nextStates: [[Bool]] = nextCellStates([
+            [true, true, true, true],
+            [true, true, true, true],
+            [true, true, true, true],
+            [true, true, true, true]
+            ])
+        let expected: [[Bool]] = [
+            [true, false, false, true],
+            [false, false, false, false],
+            [false, false, false, false],
+            [true, false, false, true]
+            
+        ]
+        XCTAssertEqual(expected, nextStates)
+    }
 }
 
 class LRUCacheTests: XCTestCase {
@@ -52,5 +108,76 @@ class LRUCacheTests: XCTestCase {
         cache.set(4, v: "Swift")
         XCTAssertNil(cache.get(3))
         XCTAssertEqual(cache.get(2)!, "World!")
+    }
+    
+    func testNoCapacity() {
+        let cache = LRUCache<Int, String>(capacity: 0)
+        cache.set(3, v: "Hello")
+        cache.set(2, v: "World!")
+        XCTAssertNil(cache.get(3))
+        XCTAssertNil(cache.get(2))
+        
+        
+        let cache2 = LRUCache<Int, String>(capacity: -1)
+        cache.set(3, v: "Hello")
+        cache.set(2, v: "World!")
+        XCTAssertNil(cache2.get(3))
+        XCTAssertNil(cache2.get(2))
+    }
+    
+    
+    func testLarge() {
+        //youtube LRU ex (https://www.youtube.com/watch?v=I9_BpSXBodU)
+        let cache = LRUCache<Int, String>(capacity: 3)
+        cache.set(2, v: "Hello")
+        cache.set(3, v: "World!")
+        cache.set(4, v: "Swift")
+        cache.set(2, v: "Hello") //hit
+
+        cache.set(1, v: "Hi!") //remove 3
+        XCTAssertEqual(cache.get(4)!, "Swift")
+        XCTAssertEqual(cache.get(2)!, "Hello")
+        XCTAssertEqual(cache.get(1)!, "Hi!")
+        
+        cache.set(3, v: "World!") //remove 4
+        XCTAssertEqual(cache.get(2)!, "Hello")
+        XCTAssertEqual(cache.get(1)!, "Hi!")
+        XCTAssertEqual(cache.get(3)!, "World!")
+        
+        cache.set(7, v: "...") //remove 2
+        XCTAssertEqual(cache.get(1)!, "Hi!")
+        XCTAssertEqual(cache.get(3)!, "World!")
+        XCTAssertEqual(cache.get(7)!, "...")
+        
+        cache.set(5, v: "???") //remove 1
+        XCTAssertEqual(cache.get(3)!, "World!")
+        XCTAssertEqual(cache.get(7)!, "...")
+        XCTAssertEqual(cache.get(5)!, "???")
+        
+        cache.set(4, v: "Swift") //remove 3
+        XCTAssertEqual(cache.get(7)!, "...")
+        XCTAssertEqual(cache.get(5)!, "???")
+        XCTAssertEqual(cache.get(4)!, "Swift")
+        
+        cache.set(3, v: "World!") //remove 7
+        XCTAssertEqual(cache.get(5)!, "???")
+        XCTAssertEqual(cache.get(4)!, "Swift")
+        XCTAssertEqual(cache.get(3)!, "World!")
+        
+        XCTAssertNil(cache.get(1))
+        XCTAssertNil(cache.get(2))
+        XCTAssertNil(cache.get(6))
+        XCTAssertNil(cache.get(7))
+        
+        
+    }
+    
+    func testNewVal() { //new value for same key - shouldnt change
+        let cache = LRUCache<Int, String>(capacity: 2)
+        cache.set(3, v: "Hello")
+        cache.set(3, v: "World!")
+        
+        XCTAssertEqual(cache.get(3)!, "Hello")
+
     }
 }
